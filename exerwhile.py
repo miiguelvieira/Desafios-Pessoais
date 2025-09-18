b789ecb3f0ac3c944feae093098b898e7ffe49cc
miiguelvieira <miguelvaraujo@gmail.com>
2025-09-18 00:14:43 -0300

    blablabla

--- exerwhile.py
+++ exerwhile.py
@@ -4,10 +4,26 @@ lenome = len(nome)
 i = 0
 
 while i <= lenome:
-    print(f"*{nome[i]}*")
+    print(f"{nome[i]}")
     i += 1
 
     if i == lenome:
         break
 
 
+
+j = 0
+new_name = ''
+
+while j < lenome:
+    letra = nome[j]
+    new_name += f'*{letra}*'
+    j += 1
+
+    print(new_name)
+
+print("="*50) 
+print(new_name)
+
+sem_bobebira = new_name.replace("*","").upper()
+print(sem_bobebira)
