fded9d464981782168d8caeaff4acd5d5ba3841a
miiguelvieira <miguelvaraujo@gmail.com>
2025-09-17 23:08:47 -0300

    Acrescentando while

--- exercicio3.py
+++ exercicio3.py
@@ -68,15 +68,20 @@ descrito, exiba a saudação apropriada. Ex.
 Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
 """
 
-hora = int(input("Que horas são? "))
+#hora = int(input("Que horas são? "))
+hora = input("Que horas são? ")
 
-if hora == 0 or hora <= 11:
-    print("Bom dia")
-elif hora == 12 or hora <= 17:
-    print("Boa tarde")
+if hora.isdigit():
+    hora1 = int(hora)
+
+    if hora1 == 0 or hora1 <= 11:
+        print("Bom dia")
+    elif hora1 == 12 or hora1 <= 17:
+        print("Boa tarde")
+    else:
+        print("Boa noite")
 else:
-    print("Boa noite")
-
+    print('Você Não digitou um número :)')
 
 
 """
---
+++ while.py
@@ -0,0 +1,34 @@
+qtd_linhas = 8
+qtd_colunas = 9
+
+linha = 0
+coluna = 0
+
+while linha <= qtd_linhas:
+    linha += 1
+    
+    while coluna <= linha:
+        coluna += 1
+    print(linha, coluna)
+    linha += 1
+        
+
+print ("=" * 20)
+print("Acabou")
+print ("=" * 20)
+
+
+i = 1
+
+while i <= 5:
+    j = 0
+    while j <= i:
+        j += 1
+        print(f"{i=} , {j=}")
+    
+    i+= 1
+
+print("acabou 2")
+
+    
+
